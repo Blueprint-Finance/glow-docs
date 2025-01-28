f801c3de741c0f37154af12c2db0340a541f842a
Nazeim <nazim@blueprintfinance.com>
2025-01-29 01:28:18 +0300

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,6 +7,9 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
+
+require('dotenv').config();
+
 const config = {
   title: 'Glow Docs',
   tagline: 'DeFi protocol',
@@ -152,6 +155,14 @@ const config = {
           },
         ],*/
       },
+
+
+              algolia: {
+                    appId: process.env.ALGOLIA_APP_ID,
+                    apiKey: process.env.ALGOLIA_API_KEY,
+                    indexName: 'glowfinance',
+                    },
+
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
