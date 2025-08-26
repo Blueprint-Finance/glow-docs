cee31b2f04d4b1df7aefd69bf3fd92ce6b11cc1c
Nazeim <nazim@blueprintfinance.com>
2025-08-27 01:05:43 +0300

    configs update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
         sitemap: {
               changefreq: 'weekly',
               priority: 0.5,
-              ignorePatterns: ['/tags/**'],
+              ignorePatterns: ['/search', '/tags/**'],
               filename: 'sitemap.xml',
 },
       }),
